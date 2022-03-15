acb3b929206663b1857f02063ef4207e4a695ba6
Rohit <rohitb333@gmail.com>
2022-03-15 13:07:05 +0530

    added uc1 file

---
+++ src/com/bridelabz/empwagecomputation/EmpWageComputation.java
@@ -0,0 +1,20 @@
+package com.bridelabz.empwagecomputation;
+
+public class EmpWageComputation {
+
+    public static void main(String[] args) {
+        {
+            final int FULL_TIME = 1;
+            int empType = (int) (Math.random() * 100) % 2;
+            if (empType == FULL_TIME)
+            {
+                System.out.println("Employee is Present");
+
+            } else
+            {
+                System.out.println("Employee is Absent");
+
+            }
+        }
+    }
+}
