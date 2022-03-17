2ca19f92a0f11e4dd339668368ade2ceedf6c6fa
Rohit <rohitb333@gmail.com>
2022-03-18 00:27:41 +0530

    added file store daily wage

--- src/com/bridelabz/EmployeeWageComputation.java
+++ src/com/bridelabz/EmployeeWageComputation.java
@@ -1,17 +1,15 @@
 package com.bridelabz;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-class CompanyEmpWage {
-    interface IEmployeeWageComputation
-    {
-        public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
+interface IEmployeeWageComputation
+{
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
 
-        public void calculateTotalWage();
-    }
-
-    class CompanyEmpWage
-    {
+    public void calculateTotalWage();
+}
+    class CompanyEmpWage {
         // instance constants
         final String COMPANY_NAME;
         final int WAGE_PER_HR;
@@ -46,79 +44,93 @@ class CompanyEmpWage {
         }
     }
 
-    class EmployeeWageComputation implements IEmployeeWageComputation
+class EmployeeWageComputation implements IEmployeeWageComputation
+{
+    // class constants
+    public static final int PART_TIME = 1;
+    public static final int FULL_TIME = 2;
+    // instance variables
+    ArrayList<CompanyEmpWage> companies;
+    HashMap<String, Integer> totalEmpWages;
+
+    public EmployeeWageComputation()
     {
-        // class constants
-        public static final int PART_TIME = 1;
-        public static final int FULL_TIME = 2;
-        // instance variables
-        ArrayList<CompanyEmpWage> companies;
+        companies = new ArrayList<>();
+        totalEmpWages = new HashMap<>();
+    }
 
-        public EmployeeWageComputation()
+    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
+    {
+        CompanyEmpWage company = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
+        companies.add(company);
+        totalEmpWages.put(companyName,0);
+    }
+
+    int generateEmployeeType()
+    {
+        return (int) (Math.random() * 100) % 3;
+    }
+
+    int getWorkingHrs(int empType)
+    {
+        switch (empType)
         {
-            companies = new ArrayList<>();
+            case FULL_TIME:
+                return 8;
+            case PART_TIME:
+                return 4;
+            default:
+                return 0;
+        }
+    }
+
+    public void calculateTotalWage()
+    {
+        for (CompanyEmpWage company : companies)
+        {
+            int totalWage = calculateTotalWage(company);
+            company.setTotalEmployeeWage(totalWage);
+            System.out.println(company);
+        }
+    }
+
+    int calculateTotalWage(CompanyEmpWage companyEmpWage)
+    {
+        System.out.println("Computation of total wage of " + companyEmpWage.COMPANY_NAME + " employee");
+        System.out.println("-----------------------------------------------------");
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+
+        int workingHrs, totalWage = 0;
+        for (int day = 1, totalWorkingHrs = 0; day <= companyEmpWage.MAX_WORKING_DAYS
+                && totalWorkingHrs <= companyEmpWage.MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
+        {
+            int empType = generateEmployeeType();
+            workingHrs = getWorkingHrs(empType);
+            int wage = workingHrs * companyEmpWage.WAGE_PER_HR;
+            totalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+        }
+        totalEmpWages.put(companyEmpWage.COMPANY_NAME, totalWage);
+        return totalWage;
+    }
+
+    void printTotalEmpWages()
+    {
+        System.out.println("The Companies and their total Employee Wages are:");
+        for (String companyName : totalEmpWages.keySet())
+        {
+            System.out.println(companyName + ": " + totalEmpWages.get(companyName));
         }
 
-        public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
-        {
-            CompanyEmpWage company = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
-            companies.add(company);
-        }
+    }
 
-        int generateEmployeeType()
-        {
-            return (int) (Math.random() * 100) % 3;
-        }
-
-        int getWorkingHrs(int empType)
-        {
-            switch (empType)
-            {
-                case FULL_TIME:
-                    return 8;
-                case PART_TIME:
-                    return 4;
-                default:
-                    return 0;
-            }
-        }
-
-        public void calculateTotalWage()
-        {
-            for (CompanyEmpWage company : companies)
-            {
-                int totalWage = calculateTotalWage(company);
-                company.setTotalEmployeeWage(totalWage);
-                System.out.println(company);
-            }
-        }
-
-        int calculateTotalWage(CompanyEmpWage companyEmpWage)
-        {
-            System.out.println("Computation of total wage of " + companyEmpWage.COMPANY_NAME + " employee");
-            System.out.println("-----------------------------------------------------");
-            System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
-
-            int workingHrs, totalWage = 0;
-            for (int day = 1, totalWorkingHrs = 0; day <= companyEmpWage.MAX_WORKING_DAYS
-                    && totalWorkingHrs <= companyEmpWage.MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
-            {
-                int empType = generateEmployeeType();
-                workingHrs = getWorkingHrs(empType);
-                int wage = workingHrs * companyEmpWage.WAGE_PER_HR;
-                totalWage += wage;
-                System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
-            }
-            return totalWage;
-        }
-
-        public static void main(String args[])
-        {
-            EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
-            employeeWageComputation.addCompany("Microsoft", 4, 30, 100);
-            employeeWageComputation.addCompany("Google", 5, 40, 170);
-            employeeWageComputation.addCompany("Apple", 9, 10, 70);
-            employeeWageComputation.addCompany("Amazon", 19, 10, 150);
-            employeeWageComputation.calculateTotalWage();
-        }
-}
+    public static void main(String args[])
+    {
+        EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
+        employeeWageComputation.addCompany("Microsoft", 4, 30, 100);
+        employeeWageComputation.addCompany("Google", 5, 40, 170);
+        employeeWageComputation.addCompany("Amazon", 19, 10, 150);
+        employeeWageComputation.calculateTotalWage();
+        employeeWageComputation.printTotalEmpWages();
+    }
+    }
